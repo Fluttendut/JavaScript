54279400a77eee67c5a90cb98866641f43e5e8a6
Ella Frost <ded_19@hotmail.com>
2023-03-03 11:51:41 +0100

    third

---
+++ js/Storetal.js
@@ -0,0 +1,21 @@
+// console.log("safd 6" + 4 > 5)
+// console.log(0/0 > -999999999999999)
+const a = -9999999999999999999999999999999999
+const b = -99999999999999999999999999999999999999999999
+const c = -99999999999999999999999999999999999999999999.5
+const d = -99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+const e = 0.2
+const f = 0.3
+const g =0.3
+
+console.log(g == e+f)
+console.log(e+f)
+
+
+
+console.log(a==c) // dette giver en afrundingsfejl ved sammenlægningen af meget store eller meget små tal
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d.length)
